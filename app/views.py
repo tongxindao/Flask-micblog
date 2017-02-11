8fb0fa8631c1bcfe98015b9e936cf0350e2c5242
tongxindao <tongxindao163@gmail.com>
2017-02-11 11:13:32 +0800

    micblog -v0.2

--- app/views.py
+++ app/views.py
@@ -1,9 +1,32 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+from flask import render_template
 from app import app
+
+'''
+以下三行代码可解决：UnicodeDecodeError: 'ascii' codec can't decode byte 0xe4 in position 0: ordinal not in range(128)
+'''
+
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return '你好，世界！'
+    user = { 'nickname': '坚果' }
+    posts = [
+        {
+            'author': { 'nickname': '张三' }, 
+            'body': '功夫瑜伽这部电影真心不错！'       
+        },
+        {
+            'author': { 'nickname': '李四' }, 
+            'body': '北京的PM 2.5还行吧！'       
+        }
+    ]
+    return render_template('index.html', 
+        title = '主页',
+        user = user,
+        posts = posts)
