de806ec125b3c2c291a836ed1cfd4e7b36ae490f
tongxindao <tongxindao163@gmail.com>
2017-05-24 08:27:38 -0400

    add noVNC for micblog

--- micblog.sh
+++ micblog.sh
@@ -1,5 +1,5 @@
 #!/bash/bin
 
 docker build -t micblog .
-docker run -d --name micblog-container -p 80:80 -p 443:443 micblog
+docker run -d --name micblog-container -p 80:80 -p 8080:8080 micblog
 docker exec micblog-container /bin/bash -c "nginx"
