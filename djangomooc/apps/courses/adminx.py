996b8b7bfb8f7b1bc35379655f161ca0ee87c5ef
tongxindao <tongxindao163@gmail.com>
2017-09-16 15:05:47 +0800

    djangomooc project commit

---
+++ djangomooc/apps/courses/adminx.py
@@ -0,0 +1,39 @@
+# _*_ coding: utf-8 _*_
+__author__ = 'tongxindao'
+__date__ = '17-9-10 下午11:19'
+
+from .models import Course, Lesson, Video, CourseResource
+
+import xadmin
+
+class CourseAdmin(object):
+    list_display = ['name', 'desc', 'detail', 'degree', 'learn_times', 'students', 'fav_nums', 'image', 'click_nums', 'add_time']
+    # 时间搜索略复杂，故略过
+    search_fields = ['name', 'desc', 'detail', 'degree', 'learn_times', 'students', 'fav_nums', 'image', 'click_nums']
+    list_filter = ['name', 'desc', 'detail', 'degree', 'learn_times', 'students', 'fav_nums', 'image', 'click_nums', 'add_time']
+
+
+class LessonAdmin(object):
+    list_display = ['course', 'name', 'add_time']
+    search_fields = ['course', 'name']
+    # 如欲在筛选器中添加外键，则须用 外键名__变量名 格式
+    list_filter = ['course__name', 'name', 'add_time']
+
+
+class VideoAdmin(object):
+    # 显示列表项
+    list_display = ['lesson', 'name', 'add_time']
+    search_fields = ['lesson', 'name']
+    list_filter = ['lesson__name', 'name', 'add_time']
+
+
+class CourseResourceAdmin(object):
+    list_display = ['course', 'name', 'download', 'add_time']
+    search_fields = ['course', 'name', 'download']
+    list_filter = ['course__name', 'name', 'download', 'add_time']
+
+
+xadmin.site.register(Course, CourseAdmin)
+xadmin.site.register(Lesson, LessonAdmin)
+xadmin.site.register(Video, VideoAdmin)
+xadmin.site.register(CourseResource, CourseResourceAdmin)
---
+++ djangomooc/apps/users/adminx.py
@@ -0,0 +1,40 @@
+# _*_ coding: utf-8 _*_
+__author__ = 'tongxindao'
+__date__ = '17-9-10 下午10:30'
+
+import xadmin
+from xadmin import views
+
+from .models import EmailVerifyRecord, Banner
+
+
+class BaseSetting(object):
+    # xadmin主题功能
+    enable_themes = True
+    # 系统主题，默认以外主题须下载
+    use_bootswatch = True
+
+
+class GlobalSettings(object):
+    site_title = "慕课后台管理系统"
+    site_footer = "慕课在线学习网"
+    # 侧边栏折叠效果开启
+    menu_style = "accordion"
+
+
+class EmailVerifyRecordAdmin(object):
+    list_display = ['code', 'email', 'send_type', 'send_time']
+    search_fields = ['code', 'email', 'send_type']
+    list_filter = ['code', 'email', 'send_type', 'send_time']
+
+
+class BannerAdmin(object):
+    list_display = ['title', 'image', 'url', 'index', 'add_time']
+    search_fields = ['title', 'image', 'url', 'index']
+    list_filter = ['title', 'image', 'url', 'index', 'add_time']
+
+
+xadmin.site.register(EmailVerifyRecord, EmailVerifyRecordAdmin)
+xadmin.site.register(Banner, BannerAdmin)
+xadmin.site.register(views.BaseAdminView, BaseSetting)
+xadmin.site.register(views.CommAdminView, GlobalSettings)
